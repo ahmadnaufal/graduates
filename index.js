2c3c570e51ea976f3ec6dc4d2708474f03aa9956
Ahmad N. F <fahmadnaufal@gmail.com>
2017-10-22 18:07:58 +0700

    add pgsql routes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cool = require('cool-ascii-faces');
+var pg = require('pg');
 var express = require('express');
 var app = express();
 
@@ -25,6 +26,20 @@ app.get('/times', function(request, response) {
     result += i + ' ';
   }
   response.send(result);
+});
+
+app.get('/db', function(request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        response.send("Error " + err);
+      } else {
+        response.render('pages/db', {results: result.rows});
+      }
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
