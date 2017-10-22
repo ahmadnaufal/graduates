f7b4a9133688236c8f7fb7a388df750b225ac14f
Ahmad N. F <fahmadnaufal@gmail.com>
2017-10-22 19:03:31 +0700

    set custom page for graduates project

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var cool = require('cool-ascii-faces');
 var pg = require('pg');
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+
+// set up bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,32 +22,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.get('/times', function(request, response) {
-  var result = '';
-  var times = process.env.TIMES || 5;
-  for (var i = 0; i < times; i++) {
-    result += i + ' ';
-  }
-  response.send(result);
-});
-
-app.get('/db', function(request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err) {
-        console.error(err);
-        response.send("Error " + err);
-      } else {
-        response.render('pages/db', {results: result.rows});
-      }
-    });
-  });
-});
+app.post('/message', function(request, response) {
+  var code = request.body.messageCode;
+  response.send(code);
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
